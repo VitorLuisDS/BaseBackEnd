a4f6b11b1b9a1739b9108445f190e2ad6bca8ce2
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-03-26 10:25:03 -0300

    Creation of BaseMap

---
+++ BaseBackEnd/BaseBackEnd.Infrastructure.Data/Mappings/Base/BaseMap.cs
@@ -0,0 +1,67 @@
+﻿using BaseBackEnd.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BaseBackEnd.Infrastructure.Data.Mappings.Base
+{
+    public static class BaseMap
+    {
+        public static void Configure<TEntity>(EntityTypeBuilder<TEntity> entity, string tableName) where TEntity : class
+        {
+            Configure(entity, tableName, new ConfigBaseMap() { CreationUserIsRequired = true, HasStatusField = true });
+        }
+
+        public static void Configure<TEntity>(EntityTypeBuilder<TEntity> entity, string tableName, ConfigBaseMap configBaseMap) where TEntity : class
+        {
+
+            if (configBaseMap.HasStatusField)
+            {
+                entity.Property("Status")
+                    .HasDefaultValueSql($"(CONVERT([bit],({(int)StatusBase.Active})))")
+                    .HasComment($"Status of {tableName}. Inactive = 0, Active=1, Blocked = 2, Deleted = 3");
+                entity.HasIndex("Status");
+            }
+
+            entity.Property("CreationDate")
+                .ValueGeneratedOnAdd()
+                .HasDefaultValueSql("(GETDATE())")
+                .HasComment("Creation Date");
+
+            entity.Property("IdCreationUser")
+                .HasColumnName("IdCreationUser")
+                .IsRequired(configBaseMap.CreationUserIsRequired)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasComment("User that created the record");
+
+            entity.Property("LastModificationDate")
+                .ValueGeneratedOnUpdate()
+                .HasDefaultValueSql("(GETDATE())")
+                .HasComment("Last Modification Date")
+                .IsRequired(false);
+
+            entity.Property("IdLastModificationUser")
+                .HasColumnName("IdLastModificationUser")
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasComment("User that last modified the record");
+
+            entity.HasOne("CreationUser")
+                .WithMany()
+                .HasForeignKey("IdCreationUser")
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName($"FK_{tableName}Creation_User");
+
+            entity.HasOne("LastModificationUser")
+                .WithMany()
+                .HasForeignKey("IdLastModificationUser")
+                .HasConstraintName($"FK_{tableName}Last_Modification_User");
+        }
+    }
+
+    public class ConfigBaseMap
+    {
+        public bool HasStatusField { get; set; } = true;
+        public bool CreationUserIsRequired { get; set; } = true;
+    }
+}

171ea8907a23f695f7512ab2cbd16e077fd32b91
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-04-23 10:07:17 -0300

    Creation of pagination classes

---
+++ BaseBackEnd/BaseBackEnd.Domain/Pagination/IPagination.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaseBackEnd.Domain.Pagination
+{
+    public interface IPagination
+    {
+        [FromQuery(Name = "currentPage")]
+        public int CurrentPage { get; set; }
+
+        [FromQuery(Name = "qtdPerPage")]
+        public int QtdPerPage { get; set; }
+    }
+}
---
+++ BaseBackEnd/BaseBackEnd.Domain/Pagination/PaginationList.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BaseBackEnd.Domain.Pagination
+{
+    public class PaginationList<TItem> : IPagination
+    {
+        [JsonProperty(PropertyName = "currentPage")]
+        public int CurrentPage { get; set; }
+
+        [JsonProperty(PropertyName = "qtdPerPage")]
+        public int QtdPerPage { get; set; }
+
+        [JsonProperty(PropertyName = "itemsTotal")]
+        public int ItemsTotal { get; set; }
+
+        [JsonProperty(PropertyName = "pagesTotal")]
+        public int PagesTotal => QtdPerPage > 0 ? (int)Math.Ceiling((decimal)ItemsTotal / QtdPerPage) : 0;
+
+        [JsonProperty(PropertyName = "items")]
+        public List<TItem> Items { get; set; }
+
+        public PaginationList(List<TItem> items, int qtdPerPage, int currentPage, int itemsTotal)
+        {
+            QtdPerPage = qtdPerPage;
+            CurrentPage = currentPage;
+            ItemsTotal = itemsTotal;
+            Items = items;
+        }
+
+        public PaginationList<TDestination> Map<TDestination>(Func<TItem, TDestination> mapFunction) =>
+            new PaginationList<TDestination>(Items.Select(mapFunction).ToList(), QtdPerPage, CurrentPage, ItemsTotal);
+    }
+}

b90fb0c0e49447bff3ce5d9c4d3d841e8ac0539f
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-05-10 11:08:45 -0300

    Creation of User and Session repositories

---
+++ BaseBackEnd/BaseBackEnd.Infrastructure.Data/Repository/Security/SessionRepository.cs
@@ -0,0 +1,36 @@
+﻿using BaseBackEnd.Domain.Entities.Security;
+using BaseBackEnd.Domain.Interfaces.Repository.Security;
+using BaseBackEnd.Infrastructure.Data.Context;
+using BaseBackEnd.Infrastructure.Data.Repository.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace BaseBackEnd.Infrastructure.Data.Repository.Security
+{
+    public class SessionRepository : RepositoryBase<Session>, ISessionRepository
+    {
+        public SessionRepository(ProjectBaseContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task AddAsync(Guid userId, bool keepConnected = false)
+        {
+            await AddAsync(new Session
+            {
+                IdUser = userId,
+                KeepConnected = keepConnected
+            });
+        }
+
+        public async Task<Session> GetSessionAndUserAsync(Guid sessionId)
+        {
+            var session = await _dbSet
+                .Include(x => x.User)
+                    .ThenInclude(x => x.Profile)
+                        .ThenInclude(x => x.ProfileModulePageFunctionalities)
+                .FirstOrDefaultAsync(x => x.Id == sessionId);
+            return session;
+        }
+    }
+}
---
+++ BaseBackEnd/BaseBackEnd.Infrastructure.Data/Repository/Security/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using BaseBackEnd.Domain.Entities.Security;
+using BaseBackEnd.Domain.Interfaces.Repository.Security;
+using BaseBackEnd.Domain.ViewModels.UserVms;
+using BaseBackEnd.Infrastructure.Data.Context;
+using BaseBackEnd.Infrastructure.Data.Repository.Base;
+using BaseBackEnd.Infrastructure.Util.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BaseBackEnd.Infrastructure.Data.Repository.Security
+{
+    public class UserRepository: RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(ProjectBaseContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<User> AuthenticateAsync(UserAuthInputVm userAuthInputVm)
+        {
+            string cryptPassword = GenerateMD5.Get(userAuthInputVm.Password);
+            return await _dbSet
+                .Include(x => x.Profile)
+                    .ThenInclude(x => x.ProfileModulePageFunctionalities)
+                .FirstOrDefaultAsync(x => x.Password == cryptPassword);
+        }
+    }
+}

1ab5f58d67ff6d847ed14dcf66cba77e398e8ff0
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-05-10 11:08:02 -0300

    Creation of cryptography class

---
+++ BaseBackEnd/BaseBackEnd.Infrastructure.Util/Cryptography/GenerateMD5.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BaseBackEnd.Infrastructure.Util.Cryptography
+{
+    public static class GenerateMD5
+    {
+        public static string Get(this string input)
+        {
+            using MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}

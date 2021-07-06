1fa0c0fcf9c08a14f1aba5e6b387216f78f9c5b8
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-07-06 08:34:25 -0300

    Creation of Default Validations

--- BaseBackEnd/BaseBackEnd.Service.Validator/Security/UserAuthInputVmValidator.cs
+++ BaseBackEnd/BaseBackEnd.Service.Validator/Security/UserAuthInputVmValidator.cs
@@ -1,6 +1,5 @@
 ﻿using BaseBackEnd.Domain.ViewModel.UserVms;
 using FluentValidation;
-using System.Text.RegularExpressions;
 
 namespace BaseBackEnd.Service.Validator.Security
 {
@@ -9,24 +8,19 @@ namespace BaseBackEnd.Service.Validator.Security
         public UserAuthInputVmValidator()
         {
             RuleFor(u => u.Login)
-                .NotEmpty()
-                .WithMessage("{PropertyName} should be provided");
+                .NotEmptyWithDefaultMessage();
 
             RuleFor(u => u.Login)
-                .Length(3, 30)
-                .WithMessage("Length of {PropertyName} should be {MinLength} to {MaxLength}");
+                .LengthWithDefaultMessage(3, 30);
 
             RuleFor(u => u.Login)
-                .Matches(new Regex("^[aA-zZ0-9_-]+$"))
-                .WithMessage("{PropertyName} is not valid");
+                .MatchesWithDefaultMessage(RegexPatterns.ValidLoginPattern);
 
             RuleFor(u => u.Password)
-                .NotEmpty()
-                .WithMessage("{PropertyName} should be provided");
+                .NotEmptyWithDefaultMessage();
 
             RuleFor(u => u.Password)
-                .Length(3, 30)
-                .WithMessage("Length of {PropertyName} should be {MinLength} to {MaxLength}");
+                .LengthWithDefaultMessage(3, 30);
         }
     }
 }

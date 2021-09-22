1626a22b25a146c54063a04e31b941c3be17293d
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-09-22 14:49:25 -0300

    Refactoring tests

--- BaseBackEnd/BaseBackEnd.Security.Tests.Domain/Entities/UserTests.cs
+++ BaseBackEnd/BaseBackEnd.Security.Tests.Domain/Entities/UserTests.cs
@@ -13,13 +13,58 @@ namespace BaseBackEnd.Security.Domain.Entities.Tests
     public class UserTests
     {
         [TestMethod()]
-        public void AddNullProfileToExistingUser()
+        public void Constructor_NullName_ThrowsException()
+        {
+            //Arrange
+            NameVO name = null;
+            LoginVO login = new LoginVO("123");
+            PasswordVO password = new PasswordVO("111111");
+
+            //Act
+            Func<object> newUser = () => new User(name, login, password);
+
+            //Assert
+            Assert.ThrowsException<NullReferenceException>(newUser);
+        }
+
+        [TestMethod()]
+        public void Constructor_NullLogin_ThrowsException()
+        {
+            //Arrange
+            NameVO name = new NameVO("Vitor");
+            LoginVO login = null;
+            PasswordVO password = new PasswordVO("111111");
+
+            //Act
+            Func<object> newUser = () => new User(name, login, password);
+
+            //Assert
+            Assert.ThrowsException<NullReferenceException>(newUser);
+        }
+
+        [TestMethod()]
+        public void Constructor_NullPassword_ThrowsException()
+        {
+            //Arrange
+            NameVO name = new NameVO("Vitor");
+            LoginVO login = new LoginVO("123");
+            PasswordVO password = null;
+
+            //Act
+            Func<object> newUser = () => new User(name, login, password);
+
+            //Assert
+            Assert.ThrowsException<NullReferenceException>(newUser);
+        }
+
+        [TestMethod()]
+        public void AddProfile_NullProfile_DoesNotAdd()
         {
             //Arrange
             var user = new User(new NameVO("Vitor"), new LoginVO("123"), new PasswordVO("111111"));
             Profile profile = null;
 
-            // Act
+            //Act
             user.AddProfile(profile);
 
             //Assert
@@ -27,13 +72,13 @@ namespace BaseBackEnd.Security.Domain.Entities.Tests
         }
 
         [TestMethod()]
-        public void AddNullSessionToExistingUser()
+        public void AddSession_NullSession_DoesNotAdd()
         {
             //Arrange
             var user = new User(new NameVO("Vitor"), new LoginVO("123"), new PasswordVO("111111"));
             Session session = null;
 
-            // Act
+            //Act
             user.AddSession(session);
 
             //Assert

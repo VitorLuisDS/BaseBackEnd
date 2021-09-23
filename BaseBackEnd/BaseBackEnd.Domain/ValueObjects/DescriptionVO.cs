3a26ed1fad3e2237be66b36a8a3e36f56243ac33
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-09-23 15:06:33 -0300

    Description VO tests

--- BaseBackEnd/BaseBackEnd.Domain/ValueObjects/DescriptionVO.cs
+++ BaseBackEnd/BaseBackEnd.Domain/ValueObjects/DescriptionVO.cs
@@ -1,12 +1,12 @@
-﻿using BaseBackEnd.Security.Domain.ValueObjects.Base;
+﻿using BaseBackEnd.Security.Domain.Flunt;
+using BaseBackEnd.Security.Domain.Rules;
+using BaseBackEnd.Security.Domain.ValueObjects.Base;
 using Flunt.Validations;
 
 namespace BaseBackEnd.Security.Domain.ValueObjects
 {
     public class DescriptionVO : ValueObjectBase
     {
-        private const int MAX_LENGTH = 100;
-
         public string Description { get; private set; }
 
         public DescriptionVO(string description)
@@ -14,7 +14,8 @@ namespace BaseBackEnd.Security.Domain.ValueObjects
             Description = description;
 
             AddNotifications(new Contract<DescriptionVO>()
-                .IsGreaterThan(Description, MAX_LENGTH, $"{nameof(Description)} should have no more than {MAX_LENGTH} chars"));
+                .Requires()
+                    .IsLowerThanWithDefaultMessage(Description, DescriptionVORules.MAX_LENGTH));
         }
     }
 }
---
+++ BaseBackEnd/BaseBackEnd.Security.Tests.Domain/ValueObjects/DescriptionVOTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BaseBackEnd.Security.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseBackEnd.Security.Domain.Rules;
+
+namespace BaseBackEnd.Security.Domain.ValueObjects.Tests
+{
+    [TestClass()]
+    public class DescriptionVOTests
+    {
+        [TestMethod()]
+        public void Constructor_LongDescription_VOIsNotValid()
+        {
+            //Arrange
+            string description = string.Concat(Enumerable.Repeat("d", DescriptionVORules.MAX_LENGTH + 1));
+
+            //Act
+            DescriptionVO descriptionVO = new DescriptionVO(description);
+
+            //Assert
+            Assert.IsFalse(descriptionVO.IsValid);
+        }
+
+        [TestMethod()]
+        public void Constructor_NullDescription_VOIsValid()
+        {
+            //Arrange
+            string description = null;
+
+            //Act
+            DescriptionVO descriptionVO = new DescriptionVO(description);
+
+            //Assert
+            Assert.IsTrue(descriptionVO.IsValid);
+        }
+
+        [TestMethod()]
+        public void Constructor_CorrectDescriptionLength_VOIsValid()
+        {
+            //Arrange
+            string description = "d";
+
+            //Act
+            DescriptionVO descriptionVO = new DescriptionVO(description);
+
+            //Assert
+            Assert.IsTrue(descriptionVO.IsValid);
+        }
+    }
+}
